4e74812160b28478fab0391fa46d33f0e906a385
eet nar <eetion@gmail.com>
2020-02-27 11:45:57 -0600

    Remove an unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -44,14 +44,10 @@ weatherForm.addEventListener('submit', (e) => {
         response.json().then(data => {
 
             if (data.error) {
-                // console.log(data.error)
                 messageOne.textContent = data.error
                 messageTwo.textContent = ''
 
             } else {
-                // console.log(data.location)
-                // console.log(data.forecast)
-
                 messageOne.textContent = data.location
                 messageTwo.textContent = data.forecast
 
